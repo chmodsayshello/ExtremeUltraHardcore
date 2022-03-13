a38dd7ac42e37264e2efb59eb2a67e49ba501631
chmodsayshello <eoonneken@gmail.com>
2022-03-13 17:39:05 +0100

    Remove one unused import

--- src/main/java/chmodsayshello/extremeultrahardcore/Listeners/Mob.java
+++ src/main/java/chmodsayshello/extremeultrahardcore/Listeners/Mob.java
@@ -11,7 +11,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
-import org.bukkit.event.entity.EntityEvent;
 import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.inventory.ItemStack;
 
